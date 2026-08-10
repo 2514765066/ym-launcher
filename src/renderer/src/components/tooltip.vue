c4bc34fc273c000ca5f23971b39c003bc93faac1
张铭洋 <2514765066@qq.com>
2026-08-10 16:44:49 +0800

    chore: 调整部分导入问题

--- src/renderer/src/components/tooltip.vue
+++ src/renderer/src/components/tooltip.vue
@@ -18,7 +18,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
-import Kbd from '@/components/ui/kbd/Kbd.vue';
+import { Kbd } from '@/components/ui/kbd';
 
 defineProps<{
   content?: string;
